50261277f23b37cae2b19efe6737192afc3a16ad
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-20 21:27:16 +0100

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -1,7 +1,7 @@
 import db from '../lib/database.js'
 
 export async function before(m, { isAdmin, isPrems, isBotAdmin }) {
-		if (m.sender.startsWith('212') || m.sender.startsWith('265')) {
+		if (m.sender.startsWith('91') || m.sender.startsWith('92')) {
 			try {
 				db.data.users[m.sender].banned = true
 			} catch (e) {
